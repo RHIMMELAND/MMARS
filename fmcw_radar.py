642d754f371bd15621fbbcf5dab817c9a0ad6fe8
RHIMMELAND <169142394+RHIMMELAND@users.noreply.github.com>
2025-03-04 10:53:06 +0100

    good version - bad noise model

--- fmcw_radar.py
+++ fmcw_radar.py
@@ -165,14 +165,16 @@ class fmcw_radar:
             ADC_samples = np.zeros((self.__N_c, self.__N_s), dtype=complex)
             for n_c in range(self.__N_c):
                 ADC_samples[n_c,:] = np.exp( 1j * ( 2 * np.pi * (f_IF) * t + n_c * phi + travel_phase ) ) # / (distance**2)
-            return ADC_samples*self.__window
+            return ADC_samples
 
         # Builds the raw radar data, based on TX-RX pairs
         for tx_idx in range(len(self.__tx_antennas)):
             for rx_idx in range(len(self.__rx_antennas)):
                 self.__raw_radar_data[tx_idx,rx_idx,:,:] = IF_signal(relative_distance, radial_velocity, phase_diff[tx_idx,rx_idx])
         recieved_power = self.__transmitted_power*self.__G*self.__lambda_c**2*self.__RCS/( (4*np.pi)**3 * relative_distance**4 )
-        self.__raw_radar_data *= np.sqrt(recieved_power) +np.random.normal(0, (self.__sigma), self.__raw_radar_data.shape)+1j*np.random.normal(0, (self.__sigma), self.__raw_radar_data.shape)
+        self.__raw_radar_data += (np.random.normal(0, (self.__sigma), self.__raw_radar_data.shape)+1j*np.random.normal(0, (self.__sigma), self.__raw_radar_data.shape))/np.sqrt(2)
+        self.__raw_radar_data[:,:] *= self.__window
+        self.__raw_radar_data *= np.sqrt(recieved_power)
     
     ##############################################################################################
 # RADAR PRESETS:
@@ -184,16 +186,14 @@ def default_77GHz_FMCW_radar(radar_position = np.array([[0], [0]])):
     if radar_position.shape != (2, 1):
         raise ValueError("Position must be a 2x1 matrix")
     # Create a default 77GHz FMCW radar, used for testing
-    default_77GHz_FMCW_radar = fmcw_radar(radar_position, SNR=[20,50])
+    default_77GHz_FMCW_radar = fmcw_radar(radar_position, SNR=[10,10])
     wavelength = default_77GHz_FMCW_radar.get_wavelength()
-    default_77GHz_FMCW_radar.add_tx_antenna(np.array([[0e-3], [0]]))
-    default_77GHz_FMCW_radar.add_tx_antenna(np.array([[2*wavelength], [0]]))
-    default_77GHz_FMCW_radar.add_tx_antenna(np.array([[4*wavelength], [0]]))
+    default_77GHz_FMCW_radar.add_tx_antenna(np.array([[-6*wavelength - 3*wavelength/4], [0]]))
+    default_77GHz_FMCW_radar.add_tx_antenna(np.array([[-4*wavelength - 3*wavelength/4], [0]]))
+    default_77GHz_FMCW_radar.add_tx_antenna(np.array([[-2*wavelength - 3*wavelength/4], [0]]))
 
-    default_77GHz_FMCW_radar.add_rx_antenna(np.array([[6*wavelength], [0]]))
-    default_77GHz_FMCW_radar.add_rx_antenna(np.array([[6*wavelength+1*wavelength/2], [0]]))
-    default_77GHz_FMCW_radar.add_rx_antenna(np.array([[6*wavelength+2*wavelength/2], [0]]))
-    default_77GHz_FMCW_radar.add_rx_antenna(np.array([[6*wavelength+3*wavelength/2], [0]]))
+    default_77GHz_FMCW_radar.add_rx_antenna(np.array([[0*wavelength/2 - 3*wavelength/4], [0]]))
+    default_77GHz_FMCW_radar.add_rx_antenna(np.array([[1*wavelength/2 - 3*wavelength/4], [0]]))
+    default_77GHz_FMCW_radar.add_rx_antenna(np.array([[2*wavelength/2 - 3*wavelength/4], [0]]))
+    default_77GHz_FMCW_radar.add_rx_antenna(np.array([[3*wavelength/2 - 3*wavelength/4], [0]]))
     return default_77GHz_FMCW_radar
-
-
