8fac0fb0b94611f32929bdd0c57260f8da1d9976
RHIMMELAND <169142394+RHIMMELAND@users.noreply.github.com>
2025-03-25 17:42:23 +0100

    Changes

--- mmars/simulation.py
+++ mmars/simulation.py
@@ -1,10 +1,10 @@
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 from .target import Target
 from .fmcwRadar import FmcwRadar
-
 
 class Simulation:
     def __init__(self, 
@@ -42,38 +42,9 @@ class Simulation:
 
 
         if self.__tracking_algorithm == "maximum_value":
-            max_range = self.__radar_setup.get_max_range()
-            N_samples = self.__radar_setup.get_N_samples()
-            range_values = np.linspace(0, max_range, N_samples)
-
-            k = 0       
-
-            for frame in tqdm(self.__frames):
-                range_fft = np.fft.fft(frame[0][0][0])
-
-                range_fft = np.abs(range_fft)
-                radial_distance = range_values[np.argmax(range_fft)]
-                
-
-                idx = self.__radar_setup.get_IF_signal().shape
-                phasors = np.zeros((idx[0]*idx[1], 256), dtype=complex)
-                for i in range(idx[0]):
-                    for j in range(idx[1]):
-                        phasors[i*(idx[0]+1)+j] = frame[i][j][0]
-                
-                R = phasors @ phasors.conj().T*(1/N_samples)
-                ### MUSIC
-                music = doa_music(R, 1, scan_angles = np.linspace(-90, 90, 1001))
-                anglebins = np.linspace(-90, 90, 1001)
-
-                detected_angle = anglebins[np.argmax(music)]
-
-                self.__tracking_data_x[k] = radial_distance*np.cos(np.deg2rad(90-detected_angle))
-                self.__tracking_data_y[k] = radial_distance*np.sin(np.deg2rad(90-detected_angle))
-                self.__tracking_data_vx[k] = 0
-                self.__tracking_data_vy[k] = 0
-
-                k = k+1
+            self.__max_val_tracking()
+        elif self.__tracking_algorithm == "mrblat":
+            self.__mrblat()
         else:
             raise ValueError("Tracking algorithm not supported")
         
@@ -96,6 +67,61 @@ class Simulation:
     def get_tracking_data(self):
         return self.__tracking_data_x, self.__tracking_data_y, self.__tracking_data_vx, self.__tracking_data_vy
     
+    def __max_val_tracking(self):
+        max_range = self.__radar_setup.get_max_range()
+        N_samples = self.__radar_setup.get_N_samples()
+        range_values = np.linspace(0, max_range, N_samples)
+
+        k = 0       
+
+        for frame in tqdm(self.__frames):
+            range_fft = np.fft.fft(frame[0][0][0])
+
+            range_fft = np.abs(range_fft)
+            radial_distance = range_values[np.argmax(range_fft)]
+            
+
+            idx = self.__radar_setup.get_IF_signal().shape
+            phasors = np.zeros((idx[0]*idx[1], 256), dtype=complex)
+            for i in range(idx[0]):
+                for j in range(idx[1]):
+                    phasors[i*(idx[0]+1)+j] = frame[i][j][0]
+            
+            R = phasors @ phasors.conj().T*(1/N_samples)
+            ### MUSIC
+            music = doa_music(R, 1, scan_angles = np.linspace(-90, 90, 1001))
+            anglebins = np.linspace(-90, 90, 1001)
+
+            detected_angle = anglebins[np.argmax(music)]
+
+            self.__tracking_data_x[k] = radial_distance*np.cos(np.deg2rad(90-detected_angle))
+            self.__tracking_data_y[k] = radial_distance*np.sin(np.deg2rad(90-detected_angle))
+            self.__tracking_data_vx[k] = 0
+            self.__tracking_data_vy[k] = 0
+
+            k = k+1
+    def __mrblat(self):
+        MRBLAT_RADAR_CPY = copy.copy(self.__radar_setup)
+        PHI = IndexFIFO(300, np.array([0,0,0,0]))
+        print(PHI.get(return_numpy=True))
+
+        for frame in tqdm(self.__frames):
+
+
+            def GET_S_VECTOR(x,y):
+                MRBLAT_RADAR_CPY.generate_S_signal(target_velocity_x=x, target_velocity_y=y)
+                return (MRBLAT_RADAR_CPY.get_S_signal()).flatten()
+            def JACOBIAN_S():
+                pass
+            def DKL(X,Y,Z_DATA):
+                S_VECTOR = GET_S_VECTOR(X,Y)
+                LAMBDA_Z = np.eye(len(S_VECTOR))
+                S_T_LAMBDA_Z_MULT = S_VECTOR.T.conj() @ LAMBDA_Z
+
+                
+            current_Z = (frame).flatten()
+
+    
 
 
 def doa_music(R, n_sig, d = 0.5, scan_angles = range(-90, 91)):
@@ -115,3 +141,23 @@ def doa_music(R, n_sig, d = 0.5, scan_angles = range(-90, 91)):
     ps = 1 / np.square(np.linalg.norm(steering_vec.conj().T @ noise_subspace, axis = 1))
 
     return 10 * np.log10(ps)
+
+class IndexFIFO:
+    def __init__(self, size, dummy_value = None):
+        self.size = size
+        self.data = [dummy_value] * size
+        self.idx = 0
+    def append(self, value):
+        self.data[self.idx] = value
+        self.idx = (self.idx + 1) % self.size 
+    def get(self, return_numpy = False):
+        if return_numpy:
+            return np.array(self.data)
+        else:
+            return self.data
+    def get_from_idx(self, idx, return_numpy = False):
+        if return_numpy:
+            return np.array(self.data[self.idx + idx])
+        else:
+            return self.data[self.idx + idx]
+        
