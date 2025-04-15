60386f391d21d9ca35a1c0eb26ba183ec59bad52
Malthe Raschke <malthe@raschke.dk>
2025-04-15 11:40:34 +0200

    Resolved mergde conflicts

--- mmars/__init__.py
+++ mmars/__init__.py
@@ -7,14 +7,3 @@ from .fmcwRadar import FmcwRadar
 from .target import Target
 from .simulation import Simulation
 from .mrblatFunctions import MRBLaT_Functions
-
-
-# from . import fmcwRadar
-# from . import target
-# from . import simulation
-# from . import mrblatFunctions_new
-
-# from .fmcwRadar import FmcwRadar
-# from .target import Target
-# from .simulation import Simulation
-# from .mrblatFunctions_new import MRBLaT_Functions_new
